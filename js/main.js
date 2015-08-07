06510e607017f845e82db663db4dfdc82f5c792b
Piotr Szczęśniak <szczesniakp@o2.pl>
2015-08-07 16:12:43 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,48 @@
+(function() {
+
+    /* onclick conversion */
+    document.getElementById('js-convert').addEventListener('click', function(){
+        var svg = document.querySelector('svg'),
+            canvas = document.createElement('canvas'),
+            body = document.getElementsByTagName('body')[0],
+            canvasTmp,
+            svg_xml;
+
+        // if our "svg" doesn't have 'xmlns' attribute we must add it
+        //svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+
+        canvas.id = 'canvas';
+        canvas.width = svg.getBoundingClientRect().width;
+        canvas.height = svg.getBoundingClientRect().height;
+
+        body.appendChild(canvas);
+        canvasTmp = document.getElementById('canvas');
+        
+        /* Time to play ;) */
+
+        // https://developer.mozilla.org/en/XMLSerializer
+        svg_xml = (new XMLSerializer()).serializeToString(svg);
+        var ctx = canvasTmp.getContext('2d');
+
+        // JavaScript (HTML) image
+        var img = new Image();
+
+        img.onload = function() {
+            ctx.drawImage(img, 0, 0);
+        };
+
+        // https://developer.mozilla.org/en/DOM/window.btoa
+        img.src = 'data:image/svg+xml;base64,' + btoa(svg_xml);
+
+        // img.src - contains base64 URI from svg
+        console.log(img.src);
+
+        //remove temporary canvas
+        canvasTmp.remove();
+
+        //Check in browsers
+        //body.appendChild(img);
+
+    });
+
+})();
